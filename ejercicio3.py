e9333227814ebf5cbb15c01d9c0364325752d20a
solrac1234-examenfinal_metodos-k8cazdgd <solrac1234-examenfinal_metodos-k8cazdgd@local>
2018-05-25 01:04:18 +0000

    se anade el ejercicio 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -4,6 +4,21 @@
 # Calcule la covarianza entre `u` y `v` e imprima su valor.
 
 import numpy as np
+import matplotlib.pyplot as plt
 t = np.array([0.,0.1,0.2,0.3,0.4,0.5,0.6, 0.8, 0.9])
 u = np.array([12.,45.,6.,78.,34.,22.,-10.,31.,-27.])
 v = np.array([3.,11.,1.3,37.,11.,6.,-23.,7.,7.])
+
+plt.figure()
+plt.plot(t,u)
+plt.plot(t,v)
+plt.show()
+
+cov = []
+promU= np.sum(u)/range(u)
+promV= np.sum(v)/range(v)
+
+for i in range(8):
+    cov = (u[i]-promU)(v[i]-promV)/(range(u)-1)
+    print(cov)
+
